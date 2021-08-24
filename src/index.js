0c76da709805dbb36ed2998c8b4ceb1a1ee0a41a
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-08-24 21:30:24 +0200

    added profile

--- src/index.js
+++ src/index.js
@@ -1,4 +1,51 @@
 // import React from 'react';
+import ReactDOM from 'react-dom';
+import data from './user.json';
+
+function Profile(props) {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={props.url} alt={props.alt} className="avatar" />
+        <p className="name">{props.profName}</p>
+        <p className="tag">{props.tag}</p>
+        <p className="location">{props.location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{props.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{props.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{props.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <Profile
+    url={data.avatar}
+    alt={data.name}
+    profName={data.name}
+    tag={data.tag}
+    location={data.location}
+    followers={data.stats.followers}
+    views={data.stats.views}
+    likes={data.stats.likes}
+  />,
+  document.querySelector('#root'),
+);
+
+// ============================================
+// import React from 'react';
 // import ReactDOM from 'react-dom';
 // import './index.css';
 // import App from './App';
