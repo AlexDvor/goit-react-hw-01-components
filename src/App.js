4f71ae959f5dafd575fc7a9be182050675868d16
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-08-25 19:14:15 +0200

    added css card

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import Profile from './components/Profile/Profile';
-import Container from './components/Container/Container';
+
 import data from './user.json';
 
 function App() {
   return (
-    <Container>
-      <Profile
-        url={data.avatar}
-        alt={data.name}
-        profName={data.name}
-        tag={data.tag}
-        location={data.location}
-        followers={data.stats.followers}
-        views={data.stats.views}
-        likes={data.stats.likes}
-      />
-    </Container>
+    <Profile
+      url={data.avatar}
+      alt={data.name}
+      profName={data.name}
+      tag={data.tag}
+      location={data.location}
+      followers={data.stats.followers}
+      views={data.stats.views}
+      likes={data.stats.likes}
+    />
   );
 }
 
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,31 +1,34 @@
 import PropTypes from 'prop-types';
+import Container from '../Container/Container';
 import s from './Profile.module.css';
 
 function Profile({ url, alt, profName, tag, location, followers, views, likes }) {
   return (
-    <div className="profile">
-      <div className="description">
-        <img src={url} alt={alt} className={s.img} />
-        <p className="name">{profName}</p>
-        <p className="tag">{tag}</p>
-        <p className="location">{location}</p>
-      </div>
+    <Container>
+      <div className="profile">
+        <div className="description">
+          <img src={url} alt={alt} className={s.img} />
+          <p className="name">{profName}</p>
+          <p className="tag">{tag}</p>
+          <p className="location">{location}</p>
+        </div>
 
-      <ul className="stats">
-        <li>
-          <span className="label">Followers</span>
-          <span className="quantity">{followers}</span>
-        </li>
-        <li>
-          <span className="label">Views</span>
-          <span className="quantity">{views}</span>
-        </li>
-        <li>
-          <span className="label">Likes</span>
-          <span className="quantity">{likes}</span>
-        </li>
-      </ul>
-    </div>
+        <ul className="stats">
+          <li>
+            <span className="label">Followers</span>
+            <span className="quantity">{followers}</span>
+          </li>
+          <li>
+            <span className="label">Views</span>
+            <span className="quantity">{views}</span>
+          </li>
+          <li>
+            <span className="label">Likes</span>
+            <span className="quantity">{likes}</span>
+          </li>
+        </ul>
+      </div>
+    </Container>
   );
 }
 
