a11592751eb29f2d6b89de05480015a4527ba269
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-08-26 16:32:51 +0200

    added css card

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,20 +1,23 @@
 // import PropTypes from 'prop-types';
 import Container from '../Container/Container';
+import s from './Statistics.module.css';
 
 function Statistics({ stats, title }) {
   return (
     <section className="statistics">
       <Container>
-        <h2 className="title">{title}</h2>
+        <div>
+          <h2 className={s.title}>{title}</h2>
 
-        <ul className="stat-list">
-          {stats.map(item => (
-            <li className="item" key={item.id}>
-              <span className="label">{item.label}</span>
-              <span className="percentage">{item.percentage}%</span>
-            </li>
-          ))}
-        </ul>
+          <ul className={s.statsList}>
+            {stats.map(item => (
+              <li className={s.item} key={item.id}>
+                <span className="label">{item.label}</span>
+                <span className={s.percentage}>{item.percentage}%</span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </Container>
     </section>
   );
