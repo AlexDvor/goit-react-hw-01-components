2b3654468c009fe6cfe8190114f36e8cadf86b5a
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-08-29 16:08:23 +0200

    updated friendList jsx

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,16 +1,13 @@
 import PropTypes from 'prop-types';
 import Container from '../Container/Container';
-import s from './FriendList.module.css';
-import FriendListItem from '../FriendListItem/FriendListItem';
+import FriendListItem from './FriendListItem';
 
 function FriendList({ friends }) {
   return (
     <Container>
-      <ul className={s.friendList}>
-        {friends.map(({ avatar, name, isOnline, id }) => (
-          <li className={s.item} key={id}>
-            <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
-          </li>
+      <ul>
+        {friends.map(({ avatar, name, isOnline }) => (
+          <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
         ))}
       </ul>
     </Container>
--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types';
 import s from './FriendListItem.module.css';
 
-function FriendListItem({ avatar, name, isOnline }) {
+function FriendListItem({ avatar, name, isOnline, id }) {
   return (
-    <>
+    <li className={s.item} key={id}>
       <span className={`${s.status} ${s[isOnline]}`}></span>
       <img className="avatar" src={avatar} alt={name} width="48" />
       <p className="name">{name}</p>
-    </>
+    </li>
   );
 }
 
